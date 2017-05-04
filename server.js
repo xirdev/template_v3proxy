83e48edf4e4f1d59931d0a51bcfb2e7878b76753
Ritchie Turner <ritchie@async.cl>
2017-05-04 12:24:38 -0300

    first

--- server.js
+++ server.js
@@ -9,122 +9,40 @@ var https = require('https')
 var http = require('http')
 var url = require('url')
 var fs = require('fs')
+var stripe = require('stripe')
+
 var bodyParser = require('body-parser');
 
-function get_config() {
-    var config_file = process.argv[2]
-
-    if (!config_file) {
-        console.log("Please provide config file name as argument")
-        process.exit(1)
-    }
-
-    if (!fs.existsSync(config_file)) {
-        console.log(config_file + " does not exist")
-        process.exit(1)
-    }
-
-    return JSON.parse(fs.readFileSync(config_file))
-}
-
-
-var conf = get_config()
-
-
-function is_cell() {
-    return conf.gateway == "NEURON"
-}
-
-
-function neuron() {
-    return process.env.NEURON_PORT_4006_TCP_ADDR
-}
-
-
-function get_gateway() {
-    return (is_cell()) ? neuron() + ":4003" : conf.gateway
-}
-
-
-function get_mount_point(app) {
-    var mountPoint;
-    if (is_cell()) {
-        // a cell requires to be on a sub-url as nginx knows about "/xsdk", mounting is a good way to achieve this
-        mountPoint = express()
-        app.use("/xsdk", mountPoint)
-        console.log("mounting on /xsdk")
-    } else {
-        mountPoint = app
-    }
-    mountPoint.use(bodyParser.json())
-    mountPoint.use(bodyParser.urlencoded({ extended: true }))
-    mountPoint.use(express.static('./public'));
-    return mountPoint
-}
+const NEURON = process.env.NEURON_PORT_4006_TCP_ADDR
+const PORT=4007
 
 var app = express();
 
-var mountPoint = get_mount_point(app),
-    gw = conf.protocol + "://" + get_gateway()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('./public'));
 
 
-if (conf.protocol == "https") {
-    var opts = {
-        key: fs.readFileSync(conf.key_file),
-        cert: fs.readFileSync(conf.cert_file)
-    };
-    https.createServer(opts, app).listen(conf.port)
-} else {
-    console.log("Http server on " + conf.port)
-    http.createServer(app).listen(conf.port)
+function service(method,service,path,data,cb) {
+    request({
+        method: method.toUpperCase(),
+        url: `http://${NEURON}:4006/${service}${path}`,
+        json: true, 
+        form: data
+    },cb)
 }
 
 
+http.createServer(app).listen(PORT)
+
+
 //Returns Secure token to connect to the service.
-mountPoint.post('/signal/token', function(req, res) {
-    body = req.body
-    body["ident"] = conf.ident
-    body["secret"] = conf.secret
-    var url = gw + "/signal/token"
-    request.post({ url: gw + "/signal/token", json: true, form: body }).pipe(res)
-})
-
-
-//Returns List of valid signaling servers that the clients can connect to.
-mountPoint.get('/signal/list', function(req, res) {
-    var secure = req.query.secure
-    if (!secure)
-        sec = ""
-    else
-        sec = "?secure=" + secure
-
-    request.get({ url: gw + "/signal/list" + sec, json: true }).pipe(res)
-})
-
-
-//Returns a Valid ICE server setup to handle the WebRTC handshake and TURN connection if needed.
-mountPoint.post('/ice', function(req, res) {
-    var body = req.body
-    body["ident"] = conf.ident
-    body["secret"] = conf.secret
-    request.post({ url: gw + "/ice", json: true, form: body }).pipe(res)
-});
-
-
-// an interesting tweak on serving different xirsys_connects for various purposes ...
-mountPoint.get('/xirsys_connect.js', function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/javascript' })
-    var xirsysConnect = {
-        secureTokenRetrieval: true,
-        server: '',
-        data: {
-            domain: 'www.xirsys.com',
-            application: 'default',
-            room: 'default'
+app.post('/deposit', function(req, res) {
+   service("GET","_acc","/accounts",{k: "demouser"},(err,status) => {
+        if (status.s == "ok") {
+            res.json(status.v)
+        } else {
+            res.json(status)
         }
-    }
-
-    var xc = "var xirsysConnect=" + JSON.stringify(xirsysConnect) + ";\n"
-
-    res.end(xc)
-});
+   })
+})
